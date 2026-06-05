f2a32111294f780ed0486bae561c96709d7fba22
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:58 -0500

    xml commenting

--- src/Soenneker.Ups.OpenApiClientUtil/Abstract/IUpsOpenApiClientUtil.cs
+++ src/Soenneker.Ups.OpenApiClientUtil/Abstract/IUpsOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Ups.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IUpsOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<UpsOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
